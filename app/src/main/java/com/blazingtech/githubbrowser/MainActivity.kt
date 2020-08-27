17877b617dabbd701c99e9e2920b466a4562d974
Chukwuwa Uchenna <Chukwuwauchenna@gmail.com>
2020-08-28 00:04:59 +0100

    hooked up the homeFragment with its viewModel

--- app/src/main/java/com/blazingtech/githubbrowser/MainActivity.kt
+++ app/src/main/java/com/blazingtech/githubbrowser/MainActivity.kt
@@ -2,6 +2,7 @@ package com.blazingtech.githubbrowser
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.blazingtech.githubbrowser.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     
@@ -9,6 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .add(R.id.screenContainer, HomeFragment())
+                .commit()
+        }
     }
 
 
--- home/src/main/java/com/blazingtech/githubbrowser/home/HomeFragment.kt
+++ home/src/main/java/com/blazingtech/githubbrowser/home/HomeFragment.kt
@@ -4,12 +4,18 @@ import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.blazingtech.githubbrowser.di.scope.ScreenScope
 import com.blazingtech.githubbrowser.di.viewmodel.AppViewModelFactory
 import com.blazingtech.githubbrowser.home.databinding.ScreenHomeBinding
+import com.blazingtech.githubbrowser.home.list.HomeRepoAdapter
 import javax.inject.Inject
 
 @ScreenScope
@@ -31,6 +37,47 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding = ScreenHomeBinding.inflate(inflater,container,false)
 
+        binding.repoList.apply {
+            adapter = HomeRepoAdapter()
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,DividerItemDecoration.VERTICAL
+                )
+            )
+        }
+
+        homeViewModel.viewStateUpdate.observe(viewLifecycleOwner, Observer {state ->
+            when(state){
+                is HomeLoadingState -> handleLoadingState(binding)
+                is HomeLoadedState -> handleLoadedState(state, binding)
+                is HomeLoadingErrorState -> handleErrorState(state, binding)
+            }
+        })
         return binding.root
+    }
+
+    private fun handleErrorState(state: HomeLoadingErrorState, binding: ScreenHomeBinding) {
+        binding.repoList.visibility = GONE
+        binding.loadingIndicator.visibility = GONE
+
+
+        binding.errorTextView.visibility = VISIBLE
+        binding.errorTextView.text = state.message
+
+    }
+
+    private fun handleLoadedState(state: HomeLoadedState, binding: ScreenHomeBinding) {
+        binding.errorTextView.visibility = GONE
+        binding.loadingIndicator.visibility = GONE
+
+        binding.repoList.visibility = VISIBLE
+        (binding.repoList.adapter as HomeRepoAdapter).setRepoItems(state.repos)
+    }
+
+    private fun handleLoadingState(binding: ScreenHomeBinding) {
+        binding.repoList.visibility = GONE
+        binding.errorTextView.visibility = GONE
+        binding.loadingIndicator.visibility = VISIBLE
     }
 }
